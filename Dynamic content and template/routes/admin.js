1c9fe92273ef8eb508542784e2e473d78d45910b
rkc <rahulchandawat22aug@gmail.com>
2020-09-16 15:49:08 +0530

    Use handlebars

--- Dynamic content and template/routes/admin.js
+++ Dynamic content and template/routes/admin.js
@@ -10,7 +10,7 @@ const products = [];
 router.get('/add-product', (req, res, next) => {
     // res.send('<form action="/admin/add-product" method="POST"> <input type="text" name="title"><button type="submit">SUBMIT</button></input></form>');
     // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', activeAddProduct: true, productCSS: true });
 });
 
 router.post('/add-product', (req, res, next) => {
