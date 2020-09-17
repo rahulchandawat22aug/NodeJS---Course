18ed9a4f9d9badee4ae02fced99cb6417193f78d
rkc <rahulchandawat22aug@gmail.com>
2020-09-17 15:22:13 +0530

    App Enhancement

--- myApp/routes/admin.js
+++ myApp/routes/admin.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 const router = express.Router();
 
 // /admin/add-product
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
 // /admin/add-product
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
+
+// /admin/products
+router.get('/products', adminController.getProducts);
 
 module.exports = router;
